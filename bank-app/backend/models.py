b015f951b6330cdd0dae500ac66ae1c448facdd0
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-06-23 22:43:52 -0400

    feat: models fundation

---
+++ bank-app/backend/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+
+from database import Base
+
+
+class Account(Base):
+    __tablename__ = "accounts"  #tells SQLAlchemy what to call the table in postgres
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True) #index=True means create an index on this column
+    owner_name = Column(String(100))
+    account_type = Column(String(20), default="chequing")
+    balance = Column(Integer, nullable=False, default=0)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    
+
