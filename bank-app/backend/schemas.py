ce13cf23e6d035103843b3df997011131871921d
Jonathan <jonathan.jf.zhao@gmail.com>
2026-06-23 23:26:14 -0400

    feat: AccountUpdate for new endpoint

--- bank-app/backend/schemas.py
+++ bank-app/backend/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
+
 """
 SQLAlchemy defines what the db table looks like
 Pydantic defines what data coming in & out of your API looks like
@@ -11,6 +13,14 @@ class AccountCreate(BaseModel):
     balance: int
 
 
+"""
+You don’t require all fields
+User can update only what they want
+"""
+class AccountUpdate(BaseModel):
+    owner_name: Optional[str] = None
+    account_type: Optional[str] = None
+    balance: Optional[int] = None
 class AccountResponse(BaseModel): #what the api sends back
     id: int
     owner_name: str
@@ -20,3 +30,4 @@ class AccountResponse(BaseModel): #what the api sends back
 
     class Config: #This lets Pydantic read SQLAlchemy objects directly instead of only reading plain dictionaries.
         from_attributes = True
+
