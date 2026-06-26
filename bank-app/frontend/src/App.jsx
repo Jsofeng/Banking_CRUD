ffd1c63144a88bf816a203a13a7b6ccd9ef9b90c
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-06-26 00:01:06 -0400

    feat: css & app configuration

---
+++ bank-app/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import AccountList from "./components/AccountList";
+import AccountForm from "./components/AccountFrom";
+
+function App() {
+  return (
+    <div>
+      <AccountForm />
+      <AccountList />
+    </div>
+  );
+}
+
+export default App;
