ff87b471f6f0622fe4f3a7eaacf49e9007260391
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-06-27 22:24:41 -0400

    feat: User Table Created for Auth

--- bank-app/backend/models.py
+++ bank-app/backend/models.py
@@ -16,3 +16,20 @@ class Account(Base):
     frozen = Column(Boolean, default=False)
 
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    username = Column(String(30), unique=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+
+    role = Column(
+        String,
+        nullable=False,
+        default="user"   # "user" or "admin"
+    )
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
