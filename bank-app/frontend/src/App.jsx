124ef4f83b71cc23bc9583b479b62fcf5606a6d9
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-03 22:55:48 -0400

    feat: renamed to AccountForm & connected Login/Register to App.jsx

--- bank-app/frontend/src/App.jsx
+++ bank-app/frontend/src/App.jsx
@@ -1,13 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./components/login";
+import Register from "./components/register";
 import AccountList from "./components/AccountList";
-import AccountForm from "./components/AccountFrom";
+import AccountForm from "./components/AccountForm";
 
 function App() {
-  return (
-    <div>
-      <AccountForm />
-      <AccountList />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+
+                <Route path="/register" element={<Register />} />
+
+                <Route
+                    path="/accounts"
+                    element={
+                        <>
+                            <AccountForm />
+                            <AccountList />
+                        </>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
