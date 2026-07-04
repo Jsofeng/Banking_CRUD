5b8f0fdbc91ffc34f54b52da33e29f168cde85cc
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-03 23:24:54 -0400

    feat: authFetch -> await fetch with token authorization

--- bank-app/frontend/src/utils/authFetch.js
+++ bank-app/frontend/src/utils/authFetch.js
@@ -3,6 +3,20 @@ export function authFetch(url, options = {}) {
 
     const isFormData = options.body instanceof FormData;
 
+    //auto fetches the url with added Authorization 
+
+    /*
+    Without authFetch:
+
+    You must tell the restaurant every time:
+
+    “Here is my name, my ID, and my payment info”
+
+    With authFetch:
+
+    You give the waiter your “membership card” once, and they handle it for every order.
+    */
+   
     return fetch(url, {
         ...options,
         headers: {
