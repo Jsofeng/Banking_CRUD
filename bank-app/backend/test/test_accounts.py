033adc99139ae7de32debc72d32d1028475bdc84
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-13 23:23:22 -0400

    test: create_account passed

---
+++ bank-app/backend/test/test_accounts.py
@@ -0,0 +1,23 @@
+import pytest
+
+@pytest.mark.asyncio
+async def test_create_account(client, authenticated_user):
+    response = await client.post(
+        "/accounts",
+        json={
+            "owner_name": "Joe",
+            "balance": 1000.0,
+            "account_type": "checking"
+        },
+        headers=authenticated_user["headers"]
+    )
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert body["owner_name"] == "Joe"
+    assert body["account_type"] == "checking"
+    assert body["balance"] == 1000.0
+
+    assert body["id"] == authenticated_user["user_id"]
