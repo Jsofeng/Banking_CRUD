bcc35414b4f202f744adc9cef26af397117b1f92
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-14 22:55:56 -0400

    test: ownership privacy test passed

--- bank-app/backend/test/test_accounts.py
+++ bank-app/backend/test/test_accounts.py
@@ -71,3 +71,76 @@ async def test_get_accounts(client, authenticated_user):
 
     assert isinstance(body, list)
     assert len(body) == 2
+
+@pytest.mark.asyncio
+async def test_account_ownership_privacy(client):
+
+    #register user 1
+    await client.post(
+        "/register",
+        json={
+            "username": "user1",
+            "email": "test1@example.com",
+            "password": "password123"
+        }
+    )
+
+    #login user 1
+    login1 = await client.post(
+        "/login",
+        data={
+            "username": "user1",
+            "password": "password123"
+        }
+    )
+
+    headers1 = {
+        "Authorization": f"Bearer {login1.json()['access_token']}"
+    }
+    
+    #register user 2
+    await client.post(
+        "/register",
+        json={
+            "username": "user2",
+            "email": "test2@example.com",
+            "password": "password123"
+        }
+    )
+
+    #login as user 2
+    login2 = await client.post(
+        "/login",
+        data={
+            "username": "user2",
+            "password": "password123"
+        }
+    )
+
+    #User 2 auth token
+    headers2 = {
+        "Authorization": f"Bearer {login2.json()['access_token']}"
+    }
+
+    #User 1 creates account
+    await client.post(
+       "/accounts",
+        json={
+            "owner_name": "User One",
+            "balance": 1000.0,
+            "account_type": "checking"
+        },
+        headers=headers1
+    )
+
+    response = await client.get(
+        "/accounts",
+        headers=headers2
+    )
+
+    assert response.status_code == 200
+    
+    body = response.json()
+
+    assert isinstance(body, list)
+    assert len(body) == 0
