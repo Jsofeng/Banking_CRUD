cb0ecd85f5548a54324c4e64250783879e5d9c4f
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-14 22:38:09 -0400

    test: get /accounts protection test passed

--- bank-app/backend/test/test_accounts.py
+++ bank-app/backend/test/test_accounts.py
@@ -24,7 +24,7 @@ async def test_create_account(client, authenticated_user):
 
 
 @pytest.mark.asyncio
-async def test_create_account_with_no_auth_headers(client):
+async def test_create_account_without_token(client):
     response = await client.post(
         "/accounts",
         json={
@@ -36,3 +36,38 @@ async def test_create_account_with_no_auth_headers(client):
 
     assert response.status_code == 401
 
+@pytest.mark.asyncio
+async def test_get_accounts(client, authenticated_user):
+    headers = authenticated_user["headers"]
+    
+    await client.post(
+        "/accounts",
+        json={
+            "owner_name": "user2",
+            "balance": 1000.0,
+            "account_type": "checking"
+        },
+        headers=headers
+    )
+
+    await client.post(
+        "/accounts",
+        json={
+            "owner_name": "user2",
+            "balance": 1000.0,
+            "account_type": "checking"
+        },
+        headers=headers
+    )
+
+    response = await client.get(
+        "/accounts",
+        headers=headers
+    )
+
+    assert response.status_code == 200
+    
+    body = response.json()
+
+    assert isinstance(body, list)
+    assert len(body) == 2
