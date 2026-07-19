4db33d6bec35d475bc7e532f1aa0673961770ced
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-18 23:14:21 -0400

    test: transaction while account is frozen passed

--- bank-app/backend/test/test_transactions.py
+++ bank-app/backend/test/test_transactions.py
@@ -40,4 +40,45 @@ async def test_get_transactions(client, authenticated_user):
     assert transactions[0]["amount"] == 1000.0
     assert transactions[0]["transaction_type"] == "deposit"
     assert transactions[0]["account_id"] == account_id
-    
+
+
+@pytest.mark.asyncio
+async def test_transactions_with_frozen_account(client, authenticated_user):
+    headers = authenticated_user["headers"]
+
+    created_response = await client.post(
+        "/accounts",
+        json={
+            "owner_name": "test_user",
+            "balance": 1000.0,
+            "account_type": "checking"
+        },
+        headers=headers
+    )
+
+    assert created_response.status_code == 200
+
+    account_id = created_response.json()["id"]
+
+    frozen = await client.patch(
+        f"/accounts/{account_id}/set_freeze",
+        json={
+            "freeze": True
+        },
+        headers=headers
+    )
+
+    assert frozen.status_code == 200
+    assert frozen.json()["frozen"] == True
+
+    transaction = await client.patch(
+        f"/accounts/{account_id}/transaction",
+        json={
+            "amount": 1000.0,
+            "transaction_type": "deposit"
+        },
+        headers=headers
+    )
+
+    assert transaction.status_code == 400
+    assert transaction.json()["detail"] == "Account is currently frozen"
