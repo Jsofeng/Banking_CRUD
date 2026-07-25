d96def4c02b5cfcea4a56adf3b673e9ddff27e44
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-24 23:15:27 -0400

    refactor: removed REDIS_URL & replaced with localhost

--- bank-app/backend/limiter.py
+++ bank-app/backend/limiter.py
@@ -14,6 +14,6 @@ load_dotenv()
 limiter = Limiter(
     key_func=get_remote_address,  # “Use the user’s IP address to track limits.”
     storage_uri=os.getenv(  # “Where should I store request counts?”
-        "REDIS_URL", "redis://localhost:6379"
+        "redis://localhost:6379"
     ),
 )
