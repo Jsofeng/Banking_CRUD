d7835b44a2021e3052e8a0c1638aaeb3aa983809
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-07-30 22:46:04 -0400

    chore: refixed .env DATABASE_URL & flake8 formatting

--- bank-app/backend/seed.py
+++ bank-app/backend/seed.py
@@ -18,17 +18,19 @@ for i in range(TOTAL):
         account_type="chequing" if i % 2 == 0 else "savings",
         balance=fake.random_int(min=0, max=100000),
         frozen=False,
-        owner_id=fake.random_int(min=1, max=2)
+        owner_id=fake.random_int(min=1, max=2),
     )
 
     accounts.append(account)
 
-    if len(accounts) == BATCH_SIZE: #SQLAlchemy inserts all 1000 in one operation. More efficient than db.add(account)
+    if (
+        len(accounts) == BATCH_SIZE
+    ):  # SQLAlchemy inserts all 1000 in one operation. More efficient than db.add(account)
         db.bulk_save_objects(accounts)
         db.commit()
-        accounts = [] #resets account array
+        accounts = []  # resets account array
 
-if accounts: #adds the final accounts
+if accounts:  # adds the final accounts
     db.bulk_save_objects(accounts)
     db.commit()
 
